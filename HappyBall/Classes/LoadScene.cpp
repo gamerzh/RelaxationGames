a1800e001abeb1a2f40c02365fcf193bbe442e4e
gamerzh <1195933889@qq.com>
2018-05-28 11:26:57 +0800

    中文注释

--- HappyBall/Classes/LoadScene.cpp
+++ HappyBall/Classes/LoadScene.cpp
@@ -23,7 +23,7 @@ bool LoadScene::init()
 
 void LoadScene::loadView() {
 	auto visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();//只有NO_BOARD模式才会有值
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();//只有NO_BOARD模式才会有值;
 
 	auto sprite = Sprite::create("HelloWorld.png");
 	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
