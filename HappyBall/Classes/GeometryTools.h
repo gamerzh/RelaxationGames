d4f4f0a61bf0d8543f02d35084eb46c90fbb1dcb
gamerzh <1195933889@qq.com>
2018-06-05 13:39:53 +0800

    pok

--- HappyBall/Classes/GeometryTools.h
+++ HappyBall/Classes/GeometryTools.h
@@ -3,13 +3,13 @@
 
 struct AngeleRange
 {
-	int startAngle;
+	int startAngle; 
 	int endAngle;
 };
 
 class GeometryTools {
 public:
-	static cocos2d::DrawNode* drawCollisionBox(cocos2d::Rect rect);
+	static cocos2d::DrawNode* drawCollisionBox(cocos2d::Rect rect  );
 	static cocos2d::DrawNode* drawCollisionCircle(cocos2d::Vec2 point,float radius);
 	static bool angleCollision(AngeleRange origin, AngeleRange current);
-};
+}; 
