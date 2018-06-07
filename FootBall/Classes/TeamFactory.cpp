259e25ed5f6a696d32e0097640eada2207bb46f2
gamerzh <1195933889@qq.com>
2018-06-07 19:58:43 +0800

    json的配置和解析

--- FootBall/Classes/TeamFactory.cpp
+++ FootBall/Classes/TeamFactory.cpp
@@ -19,11 +19,42 @@ std::vector<FootManTeam> TeamFactory::getFootManTeamVector() {
 }
 
 std::vector<FootManTeamProperty> TeamFactory::getFootManTeamPropertyVector() {
+	std::vector<FootManTeamProperty> footManTeamPropertyVec;
 	//解析json,获取战队数据
 	string data = FileUtils::getInstance()->getStringFromFile("team.json");
 	RETURN_IF(NULL == data.c_str() || !data.compare(""));
 	rapidjson::Document _mDoc;
 	_mDoc.Parse<0>(data.c_str());
 	RETURN_IF(_mDoc.HasParseError() || !_mDoc.IsObject());
-	const rapidjson::Value &code = _mDoc["team"];
-}
+	if (_mDoc.HasMember("team")) {
+		const rapidjson::Value &team = _mDoc["team"];
+		for (int i = 0; i < team.Capacity(); i++) {
+			const rapidjson::Value &tempteam = team[i];
+			FootManTeamProperty property;
+			if (tempteam.HasMember("teamId")) {
+				property.teamId = tempteam["teamId"].GetInt();
+			}
+			if (tempteam.HasMember("teamName")) {
+				property.teamName = tempteam["teamName"].GetString();
+			}
+			if (tempteam.HasMember("teamBanner")) {
+				property.teamBanner = tempteam["teamBanner"].GetString();
+			}
+			if (tempteam.HasMember("footManVec")) {
+				const rapidjson::Value &footVec = tempteam["footManVec"];
+				for (int j = 0; j < footVec.Capacity(); j++)
+				{
+					const rapidjson::Value &tempfoot = footVec[i];
+					FootManProperty manProperty;
+					manProperty.name = tempfoot["name"].GetString();
+					manProperty.footImage = tempfoot["footImage"].GetString();
+					manProperty.runSpeed = tempfoot["runSpeed"].GetInt();
+					manProperty.skillType = tempfoot["skillType"].GetInt();
+					property.footManVec.push_back(manProperty);
+				}
+			}
+			footManTeamPropertyVec.push_back(property);
+		}
+	}
+	
+} 
