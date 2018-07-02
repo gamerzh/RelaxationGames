11ff3206cc2057c023768b51ad97092a15031722
gamerzh <1195933889@qq.com>
2018-07-02 17:48:52 +0800

    缩放策略修改

--- FootBall/Classes/AppDelegate.cpp
+++ FootBall/Classes/AppDelegate.cpp
@@ -1,7 +1,7 @@
 #include "AppDelegate.h"
 #include "LoadScene.h"
 
- #define USE_AUDIO_ENGINE 1
+//#define USE_AUDIO_ENGINE 1
 // #define USE_SIMPLE_AUDIO_ENGINE 1
 
 #if USE_AUDIO_ENGINE && USE_SIMPLE_AUDIO_ENGINE
@@ -18,21 +18,16 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(1280, 720);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
-
 AppDelegate::AppDelegate()
 {
 }
 
-AppDelegate::~AppDelegate() 
+AppDelegate::~AppDelegate()
 {
 #if USE_AUDIO_ENGINE
-    AudioEngine::end();
+	AudioEngine::end();
 #elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::end();
+	SimpleAudioEngine::end();
 #endif
 }
 
@@ -40,88 +35,73 @@ AppDelegate::~AppDelegate()
 // it will affect all platforms
 void AppDelegate::initGLContextAttrs()
 {
-    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+	// set OpenGL context attributes: red,green,blue,alpha,depth,stencil
+	GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8 };
 
-    GLView::setGLContextAttrs(glContextAttrs);
+	GLView::setGLContextAttrs(glContextAttrs);
 }
 
 // if you want to use the package manager to install more packages,  
 // don't modify or remove this function
 static int register_all_packages()
 {
-    return 0; //flag for packages manager
+	FileUtils::getInstance()->addSearchPath("res");
+	SpriteFrameCache::getInstance()->addSpriteFramesWithFile("zhujue0.plist", "zhujued0.png");
+	return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("FootBall", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
-#else
-        glview = GLViewImpl::create("HappyBall");
+	// initialize director
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
+		glview = GLViewImpl::create("HappyBall");
+		director->setOpenGLView(glview);
+	}
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+	glview->setFrameSize(1280, 720);
+	glview->setDesignResolutionSize(1280,720, ResolutionPolicy::SHOW_ALL);
 #endif
-        director->setOpenGLView(glview);
-    }
+#if (CC_TARGET_PLATFORM==CC_PLATFORM_MAC)
+	glview->setFrameSize(960, 540);
+	glview->setDesignResolutionSize(1280, 720, ResolutionPolicy::EXACT_FIT);
+#endif
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	glview->setDesignResolutionSize(1280, 720, ResolutionPolicy::SHOW_ALL);
+#endif
+	// turn on display FPS
+	//director->setDisplayStats(true);
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+	register_all_packages();
+	// create a scene. it's an autorelease object
+	auto scene = LoadScene::createScene();
+	// run
+	director->runWithScene(scene);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0f / 60);
-
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-    auto frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-    }
-
-    register_all_packages();
-
-    // create a scene. it's an autorelease object
-    auto scene = LoadScene::createScene();
-
-    // run
-    director->runWithScene(scene);
-
-    return true;
+	return true;
 }
 
 // This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
 void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
+	Director::getInstance()->stopAnimation();
 
 #if USE_AUDIO_ENGINE
-    AudioEngine::pauseAll();
+	AudioEngine::pauseAll();
 #elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
-    SimpleAudioEngine::getInstance()->pauseAllEffects();
+	SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+	SimpleAudioEngine::getInstance()->pauseAllEffects();
 #endif
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+	Director::getInstance()->startAnimation();
 
 #if USE_AUDIO_ENGINE
-    AudioEngine::resumeAll();
+	AudioEngine::resumeAll();
 #elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
-    SimpleAudioEngine::getInstance()->resumeAllEffects();
+	SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+	SimpleAudioEngine::getInstance()->resumeAllEffects();
 #endif
 }
