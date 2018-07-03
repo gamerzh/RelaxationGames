207112e38cbee64d124e1a4695c523cf88b4ba80
gamerzh <1195933889@qq.com>
2018-07-03 10:51:58 +0800

    添加计分和能量条的显示

--- FootBall/Classes/Setting.cpp
+++ FootBall/Classes/Setting.cpp
@@ -15,20 +15,72 @@ bool Setting::init() {
 void Setting::loadView() {
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
-	auto teamIcon = Sprite::create("team_icon_1.png");
-	teamIcon->setPosition(200,680);
-	addChild(teamIcon);
+
+	auto teamIcon1 = Sprite::create("team_icon_1.png");
+	teamIcon1->setPosition(280,665);
+	addChild(teamIcon1);
+
+	auto score1 = LabelAtlas::create("00","num_gold.png",71,81,'0');
+	score1->setAnchorPoint(Point::ANCHOR_MIDDLE);
+	score1->setPosition(430, 660);
+	addChild(score1);
+
+	auto timeBg = Sprite::create("time_bg.png");
+	timeBg->setPosition(visibleSize.width / 2, visibleSize.height - 60);
+	addChild(timeBg);
 
 	auto showtime = Label::createWithSystemFont(StringUtils::format("%.1f", gameTime), "arial", 40);
-	showtime->setTag(100);
+	showtime->setAnchorPoint(Point::ANCHOR_MIDDLE);
 	showtime->setColor(Color3B::WHITE);
-	showtime->setPosition(visibleSize.width / 2, visibleSize.height - 40);
+	showtime->setPosition(visibleSize.width / 2, visibleSize.height - 50);
 	addChild(showtime);
 
+	auto score2 = LabelAtlas::create("00", "num_gold.png", 71, 81, '0');
+	score2->setAnchorPoint(Point::ANCHOR_MIDDLE);
+	score2->setPosition(850, 660);
+	addChild(score2);
 
 	auto teamIcon2 = Sprite::create("team_icon_1.png");
-	teamIcon2->setPosition(1080, 680);
+	teamIcon2->setPosition(1000, 665);
 	addChild(teamIcon2);
+
+	//pause
+	auto pauseImg = MenuItemImage::create("pause_btn.png", "pause_btn.png",CC_CALLBACK_0(Setting::pauseGame,this));
+	auto pauseMenu = Menu::create(pauseImg,NULL);
+	pauseMenu->setPosition(visibleSize.width-140, visibleSize.height - 50);
+	addChild(pauseMenu);
+
+	//选中状态精灵
+	auto musicOnSpr = Sprite::create("sound_on.png");
+	//未选中状态精灵
+	auto musicOffSpr = Sprite::create("sound_off.png");
+	auto musicOn = MenuItemSprite::create(musicOnSpr, musicOnSpr);
+	auto musicOff = MenuItemSprite::create(musicOffSpr, musicOffSpr);
+	//创建MenuItemToggle，第一个参数是回调函数 后面依次是要显示的MenuItem，最后必须为NULL
+	musicItemToggle = MenuItemToggle::createWithCallback(CC_CALLBACK_1(Setting::soundMenuCallBack, this), musicOn, musicOff, NULL);
+	//if (UserData::getInstance()->getMusicStatus() == 0) {
+	//	musicItemToggle->setSelectedIndex(1);
+	//}
+	auto menu = Menu::create(musicItemToggle, NULL);
+	menu->setPosition(visibleSize.width-60, visibleSize.height - 50);
+	//menu->setVisible(false);
+	this->addChild(menu);
+
+}
+
+
+void Setting::soundMenuCallBack(Ref* ref) {
+
+}
+
+
+void Setting::showTeamScore(int teamId) {
+
+
+}
+
+void Setting::pauseGame() {
+
 
 }
 
--- FootBall/Classes/Setting.h
+++ FootBall/Classes/Setting.h
@@ -7,9 +7,13 @@ public:
 	virtual bool init();
 	CREATE_FUNC(Setting);
 private:
+	cocos2d::MenuItemToggle * musicItemToggle;
 	float gameTime = 60;//”Œœ∑ ±º‰
 	float passTime = 0;
 	const float timestep = 0.1f;
 	void loadView();
+	void showTeamScore(int teamId);
+	void soundMenuCallBack(Ref* ref);
+	void pauseGame();
 	void update(float dt);
 };
