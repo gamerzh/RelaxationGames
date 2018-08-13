ea0509e61fb38af483a9c06666444ec77b15d35c
gamerzh <1195933889@qq.com>
2018-08-13 13:59:47 +0800

    添加球门类

--- FootBall/Classes/Goal.cpp
+++ FootBall/Classes/Goal.cpp
@@ -3,13 +3,17 @@
 //  FootBall
 //
 //
-
 #include "Goal.h"
-
+#include "Ball.h"
+USING_NS_CC;
 
 bool Goal::init(){
     if(!Node::init()){
         return false;
     }
     return true;
+}
+
+cocos2d::Rect getGoalRect(cocos2d::Vec2 dir){
+    return Rect(0,0,0,0);
 }
--- FootBall/Classes/Goal.h
+++ FootBall/Classes/Goal.h
@@ -10,5 +10,9 @@ class Goal : public cocos2d::Node{
 public:
     virtual bool init();
     CREATE_FUNC(Goal);
+    cocos2d::Rect getGoalRect(cocos2d::Vec2 dir);
+private:
+    cocos2d::Vec2 goalDirector;//球门方向,左球门(1,0),右球门(-1,0)
+    cocos2d::Rect goalRect;//球门范围
 };
 #endif /* Goal_h */
