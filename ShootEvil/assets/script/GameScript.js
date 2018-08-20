f20260ee3f870534fe286224abde1792aae4efa6
gamerzh <1195933889@qq.com>
2018-08-20 09:55:12 +0800

    添加点击事件的监听

--- ShootEvil/assets/script/GameScript.js
+++ ShootEvil/assets/script/GameScript.js
@@ -9,46 +9,66 @@
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
 
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-    },
+  properties: {
+    // foo: {
+    //     // ATTRIBUTES:
+    //     default: null,        // The default value will be used only when the component attaching
+    //                           // to a node for the first time
+    //     type: cc.SpriteFrame, // optional, default is typeof default
+    //     serializable: true,   // optional, default is true
+    // },
+    // bar: {
+    //     get () {
+    //         return this._bar;
+    //     },
+    //     set (value) {
+    //         this._bar = value;
+    //     }
+    // },
+  },
 
-    // LIFE-CYCLE CALLBACKS:
+  // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
-        var physicsManager = cc.director.getPhysicsManager();
-        physicsManager.enabled = true;
-        physicsManager.debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
-        cc.PhysicsManager.DrawBits.e_pairBit |
-        cc.PhysicsManager.DrawBits.e_centerOfMassBit |
-        cc.PhysicsManager.DrawBits.e_jointBit |
-        cc.PhysicsManager.DrawBits.e_shapeBit;
+  onLoad() {
+    var physicsManager = cc.director.getPhysicsManager();
+    physicsManager.enabled = true;
+    physicsManager.debugDrawFlags =
+      cc.PhysicsManager.DrawBits.e_aabbBit |
+      cc.PhysicsManager.DrawBits.e_pairBit |
+      cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+      cc.PhysicsManager.DrawBits.e_jointBit |
+      cc.PhysicsManager.DrawBits.e_shapeBit;
 
-        var collisionManager = cc.director.getCollisionManager();
-        collisionManager.enabled = true;
-        collisionManager.enabledDebugDraw = true;
+    var collisionManager = cc.director.getCollisionManager();
+    collisionManager.enabled = true;
+    collisionManager.enabledDebugDraw = true;
+    this.touchEvent();
+  },
 
-    },
+  start() {
+    //TODO:
+  },
 
-    start () {
-        // console.log("AAAAAAAAAAAAA");
-    },
+  update(dt) {},
 
-    // update (dt) {},
+  touchEvent() {
+    // 使用枚举类型来注册
+    this.node.on(
+      cc.Node.EventType.TOUCH_START,
+      function(event) {
+        console.log("Mouse down");
+      },
+      this
+    );
+    //
+    this.node.on(
+      cc.Node.EventType.MOUSE_DOWN,
+      function(event) {
+        console.log("Mouse down ...");
+      },
+      this
+    );
+  }
 });
