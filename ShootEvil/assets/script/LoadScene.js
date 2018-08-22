09d2155957a175cc3310ee0b538bc4c0b0451618
gamerzh <1195933889@qq.com>
2018-08-22 15:38:11 +0800

    添加一个按钮到load场景

---
+++ ShootEvil/assets/script/LoadScene.js
@@ -0,0 +1,54 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.director.preloadScene("GameScene", function () {
+            cc.log("Next scene preloaded");
+        });
+        this.node.on('click', this.callback, this);
+    },
+
+    start () {
+      
+    },
+
+    callback: function (event) {
+        //这里的 event 是一个 EventCustom 对象，你可以通过 event.detail 获取 Button 组件
+        var button = event.detail;
+        //do whatever you want with button
+        //另外，注意这种方式注册的事件，也无法传递 customEventData
+        cc.director.loadScene("GameScene");
+     },
+
+    // update (dt) {},
+});
