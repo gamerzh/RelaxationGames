b528f0bbd66ad82896f4f773873bf1a59d1e87e6
gamerzh <1195933889@qq.com>
2018-08-23 13:42:14 +0800

    界面修改

--- ShootEvil/assets/script/GuiLayer.js
+++ ShootEvil/assets/script/GuiLayer.js
@@ -39,5 +39,19 @@ cc.Class({
 
     // update (dt) {},
 
-   
+    selectLevelOne: function (event) {
+        //这里的 event 是一个 EventCustom 对象，你可以通过 event.detail 获取 Button 组件
+        var button = event.detail;
+        //do whatever you want with button
+        //另外，注意这种方式注册的事件，也无法传递 customEventData
+        console.log("select level one");
+     },
+
+     selectLevelTwo:function (event){
+        console.log("select level two");
+     },
+
+     selectLevelThree:function (event){
+        console.log("select level three");
+    }
 });
